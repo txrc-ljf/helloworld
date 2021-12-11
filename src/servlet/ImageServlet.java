220b6ef83e3dcd9778c6937a81ce0e9cac89bade
txrc-ljf <416354295@qq.com>
2021-12-11 13:01:36 +0800

    新建分支后提交

--- src/servlet/ImageServlet.java
+++ src/servlet/ImageServlet.java
@@ -11,7 +11,7 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class ImageServlet extends HttpServlet {
-    private Font font =new Font("Courier",Font.BOLD,12);
+    public Font font =new Font("Courier",Font.BOLD,12);
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
